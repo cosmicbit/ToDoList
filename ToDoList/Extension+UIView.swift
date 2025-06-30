9a07277588b81fdf70ae029ef54a8277cde26473
Philips <philipsjose2003@gmail.com>
2025-06-30 14:30:03 +0530

    Grouped Files

---
+++ ToDoList/Extension+UIView.swift
@@ -0,0 +1,8 @@
+//
+//  Extension+UIView.swift
+//  ToDoList
+//
+//  Created by Philips on 30/06/25.
+//
+
+import Foundation
---
+++ ToDoList/Models/TaskModel.swift
@@ -0,0 +1,18 @@
+//
+//  TaskModel.swift
+//  ToDoList
+//
+//  Created by Philips on 25/06/25.
+//
+
+import Foundation
+
+struct Task {
+    
+    let id: String
+    let category: Category
+    let caption: String
+    let createdDate: Date
+    var isComplete: Bool
+    
+}
